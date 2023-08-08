58704c99a9b888e8350ffd6abc371ea2461c9584
sssmirnov222 <sssmirnov1993@mail.ru>
2023-08-08 18:45:46 +0300

    Сделал))

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-let link = document.querySelectorAll(".main .main__link a");
+
 let link_index = document.querySelectorAll(".main .main__link a");
-let first__link = document.querySelectorAll(".main .main__link .main-first__link ");
-let counter = 0;
+console.log(link_index);
 
-link.forEach((elem) => {
-  
-  elem.addEventListener("click", () => {
-   
-   counter++;
-    link_index.forEach((el) => {
-       
-      if (elem.innerHTML == el.innerHTML) {
 
-        if(counter %2 == 1) {
-            elem.classList.add("active");
-            elem.classList.remove("noactive");
-        }
-        if(counter %2 == 0) {
-          elem.classList.remove("active");
-          elem.classList.add("noactive");
-      }
-        
-      }
-  
-
-    });
-  });
-});
-
-// first__link.addEventListener("click", () => {
-//   counter++;
-//   if(counter %2 == 0) {
-              
-//     first__link.classList.remove(".main-first__link");
-// }
-
-// if(counter %2 == 1) {
-//   first__link.classList.add(".main-first__link");
-// }
-// })
+link_index.forEach(el => {
+  el.addEventListener("click", () => { 
+       link_index.forEach(e => {
+         console.log(e)
+         e.classList.remove("active")
+       })
+     el.classList.add("active")
+     
+      
+    
+  })
+})
