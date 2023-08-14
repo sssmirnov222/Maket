585e8b47f0e422cb955f7f76384f5568cb495470
sssmirnov222 <sssmirnov1993@mail.ru>
2023-08-14 21:13:15 +0300

    code_foot

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
+let link = document.querySelectorAll(".main .main__link a");
 let link_index = document.querySelectorAll(".main .main__link a");
-console.log(link_index);
+let counter = 0;
 
 
 link_index.forEach(el => {
@@ -15,3 +15,33 @@ link_index.forEach(el => {
     
   })
 })
+
+
+let modal = document.querySelector('.header__modal');
+let burger = document.querySelector('.header .header__left .header-burger__left img')
+let exit = document.querySelector('.header__modal .header-head__modal .exit')
+let CPS = document.querySelector('.main .main__description img')
+let text = document.querySelector('.main .text');
+console.log(text)
+
+
+// burger.addEventListener('click', () => {
+//    counter++;
+//    if(counter %2 == 1) {
+//     modal.style.display = "block";
+//     CPS.style.position = "static"
+    
+//    } 
+
+//    exit.addEventListener('click', () => {
+//     counter++;
+//     if(counter %2 == 0) {
+//     modal.style.display = "none";
+//     CPS.style.position = "absolute"
+//      }
+   
+//    })
+   
+// })
+
+console.log(CPS)
