37e4318125331f9ca56ba535725caf5db1d9daf2
sssmirnov222 <sssmirnov1993@mail.ru>
2023-09-15 09:11:21 +0300

    Finally_15_09

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ link_index.forEach(el => {
   })
 })
 
-
+// модалка
 let modal = document.querySelector('.header__modal');
 let burger = document.querySelector('.wrapper .header .header__left .header-burger__left img')
 let exit = document.querySelector('.header__modal .header-head__modal .exit')
@@ -55,7 +55,7 @@ burger.addEventListener('click', () => {
 console.log(CPS)
 
 
-
+// скрыть и показать элементы
 let down = document.querySelector('.main__servicbrands .down');
 let up = document.querySelector('.main__servicbrands .up');
 let lenovo = document.querySelector('.main__servicbrands .main-devices .lenovo_two');
@@ -106,14 +106,26 @@ let swiper = new Swiper('.swiper-container', {
       slidesPerView:1.3,
       spaceBetween: 16
     },
+
+    420: {
+      slidesPerView:1.8,
+      spaceBetween: 16
+    },
+
+    650: {
+      slidesPerView:2.7,
+      spaceBetween: 16
+    },
   }
 });
 
 
 
 //feedback
-let call = document.querySelector(".header__modal .footer .header-foot__modal .call")
+let call = document.querySelector(".aside__modal .footer .aside-foot__modal .call")
+console.log(call)
 let feedback = document.querySelector(".header__feedback")
+console.log(feedback)
 let back = document.querySelector(".header-back__feedback")
 console.log(back)
 
@@ -140,8 +152,11 @@ call.addEventListener("click", () =>  {
 )
 
 
+
+
+
 // call
-let chat = document.querySelector(".header__modal .footer .header-foot__modal .Chat")
+let chat = document.querySelector(".aside__modal .footer .aside-foot__modal .Chat")
 let headerCall = document.querySelector(".header__call")
 let headerBack = document.querySelector(".header-back__call")
 console.log(back)
@@ -161,6 +176,121 @@ chat.addEventListener("click", () =>  {
   if(counter === 0)  {
    headerCall.style.display = "none";
    main.style.opacity = "1";
+   modal.style.opacity = "1";
+    }
+  })
+
+ }
+)
+
+
+
+
+// переключение для aside
+let listAside = document.querySelectorAll(".aside__modal ul li a span");
+
+listAside.forEach(el => {
+  el.addEventListener("click",() => {
+
+     listAside.forEach(e => {
+      e.classList.remove("grey")
+     })
+   el.classList.add("grey")
+
+  })
+ 
+})
+
+let imgAside = document.querySelector(".aside__modal ul img");
+
+
+let listAs = document.querySelectorAll(".aside__modal ul li a");
+listAs.forEach(el => {
+  el.addEventListener("click",() => {
+    
+    
+   el.before(imgAside)
+  })
+})
+
+
+
+
+
+
+
+//переключение
+let listModal = document.querySelectorAll(".header__modal ul li a span");
+console.log(listModal)
+
+listModal.forEach(el => {
+  el.addEventListener("click",() => {
+
+     listModal.forEach(e => {
+      e.classList.remove("grey")
+     })
+   el.classList.add("grey")
+
+  })
+ 
+})
+
+let img = document.querySelector(".header__modal ul img");
+
+
+let list = document.querySelectorAll(".header__modal ul li a");
+list.forEach(el => {
+  el.addEventListener("click",() => {
+    
+    
+   el.before(img)
+  })
+})
+
+
+
+// показать feedback и call на 320
+let callMod = document.querySelector('.header__modal .footer .header-foot__modal .callMod')
+
+callMod.addEventListener("click", () =>  {
+  counter+=1;
+  console.log(counter)
+  if(counter) {
+   feedback.style.display = "block" 
+   feedback.style.opacity = "1";
+   main.style.opacity = "0.1";
+   modal.style.opacity = "0.1";
+  }
+  
+  back.addEventListener("click", () => {
+  counter = 0;
+  if(counter === 0)  {
+   feedback.style.display = "none";
+   main.style.opacity = "0.1";
+   modal.style.opacity = "1";
+    }
+  })
+
+ }
+)
+
+
+
+let ChatMod = document.querySelector('.header__modal .footer .header-foot__modal .ChatMod')
+ChatMod.addEventListener("click", () =>  {
+  counter+=1;
+  if(counter) {
+   headerCall.style.display = "block" 
+   headerCall.style.opacity = "1";
+   main.style.opacity = "0.1";
+   modal.style.opacity = "0.1";
+  }
+  
+  headerBack.addEventListener("click", () => {
+  counter = 0;
+  if(counter === 0)  {
+   headerCall.style.display = "none";
+   main.style.opacity = "0.1";
    modal.style.opacity = "1";
     }
   })
