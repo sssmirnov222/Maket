c8695f5df195bf62d58f8c91762b1c0966a2c5a6
sssmirnov222 <sssmirnov1993@mail.ru>
2023-09-20 19:50:04 +0300

    Webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-import '../scss/style.scss'
+import '../../node_modules/focus-visible/dist/focus-visible';
+import '../scss/main.scss';
+import '../index.html';
 import Swiper, { Pagination } from 'swiper';
 Swiper.use([Pagination]);
 
